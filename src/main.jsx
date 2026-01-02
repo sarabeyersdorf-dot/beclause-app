db3f3d578a0ca3e87b49f58cf3f01967f7d1e666
sarabeyersdorf-dot <sarabeyersdorf@gmail.com>
2026-01-01 16:03:21 -0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import TxnUploadPage from './pages/TxnUploadPage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/transactions/:id" element={<TxnUploadPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
